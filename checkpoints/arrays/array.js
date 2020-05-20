cadaf5779704f708ee3fc48c42d767ea46ff3208
AlejandroEscamilla <alejandroe.ordaz@gmail.com>
2020-05-19 21:56:01 -0400

    React Submission

---
+++ checkpoints/arrays/array.js
@@ -0,0 +1,20 @@
+var drinks = [];
+
+drinks.push('water');
+drinks.push('coffee');
+drinks.push('tea');
+
+document.write(drinks)
+
+var foods = [];
+foods.push('apple');
+foods.push('sandwich');
+foods.push('chips');
+
+document.write(foods)
+
+var meals = [];
+meals.push = (drinks);
+meals.push = (foods);
+
+document.write(meals);
